0af7dc3b6447fc782859fdee4764120ed2e143f8
Yury Shkliaryk <joraillusion@yandex.by>
2015-12-29 21:27:08 +0300

    Adding multitype supporting

--- py.py
+++ py.py
@@ -2,23 +2,21 @@ from __future__ import unicode_literals
 import os
 import re
 
-REGEX_FOR_DEFINITIONS = r'(?<=\\begin{definition})[\w\W]+?(?=\\end{definition})'
-REGEX_FOR_PROPERTIES = r'(?<=\\begin{property})[\w\W]+?(?=\\end{property})'
-REGEX_FOR_THEOREMS = r'(?<=\\begin{theorem})[\w\W]+?(?=\\end{theorem})'
-REGEX_FOR_STATEMENTS = r'(?<=\\begin{statement})[\w\W]+?(?=\\end{statement})'
-
-MATCHER = re.compile(REGEX_FOR_DEFINITIONS)
-template = u'{} "[latex]{}[/latex]" "functional_analysis definition"\n'
+TYPES = ["definition", "property", "theorem", "statement"]
+REGEX_TEMPLATE = r'(?<=\\begin{{{0}}})[\w\W]+?(?=\\end{{{0}}})'
+CARD_TEMPLATE = u'{} "[latex]{}[/latex]" "functional_analysis {}"\n'
 COUNTER = 0
 
 
 def gen_cards(text):
     global COUNTER
-    raw_tokens = MATCHER.finditer(text)
-    for raw_token in raw_tokens:
-        answer_field = re.sub(u'\\label{[\w\W]+?}', u'', unicode(raw_token.group(0).decode('cp1251')))
-        yield template.format(str(COUNTER), answer_field)
-        COUNTER += 1
+    for type in TYPES:
+        matcher = re.compile(REGEX_TEMPLATE.format(type))
+        raw_tokens = matcher.finditer(text)
+        for raw_token in raw_tokens:
+            answer_field = re.sub(u'\\label{[\w\W]+?}', u'', unicode(raw_token.group(0).decode('cp1251')))
+            yield CARD_TEMPLATE.format(str(COUNTER), answer_field, type)
+            COUNTER += 1
 
 
 def get_cards(dir):
